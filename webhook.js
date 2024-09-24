838b0b8c781a8bb948f932872484238333e72a76
AjUm-HEIDI <ajay@heidi-app.com>
2024-09-24 15:15:53 +0200

    Update webhook

--- webhook.js
+++ webhook.js
@@ -3,29 +3,30 @@ require("dotenv").config();
 
 const webhookUrl = `http://localhost:${process.env.WEBHOOK_PORT}/publish/barcode?accessToken=${process.env.ACCESS_TOKEN}`;
 
-function sendDiscordWebhook(data, type) {
-    return new Promise((resolve, reject) => {
-        let embedTitle, fields;
+async function sendDiscordWebhook(data, type) {
+    console.log(webhookUrl)
+    let payloadData;
 
-        if (type === 'nfc') {
-            embedTitle = 'NFC Card Scanned';
-            fields = [{ name: 'UID', value: data.uid }, { name: 'Block Data', value: data.blockData }];
-        } else if (type === 'barcode') {
-            embedTitle = 'Barcode Scanned';
-            fields = [{ name: 'Barcode Data', value: data.barcodeData }];
-        } else {
-            return reject(new Error('Unknown notification type'));
-        }
+    if (type === 'nfc') {
+        const embedTitle = 'NFC Card Scanned'; // Local variable 'embedTitle' wasn't declared
+        const fields = [{ name: 'UID', value: data.uid },
+                        { name: 'Block Data', value: data.blockData }];
+        payloadData = { embeds: [{ title: embedTitle, fields: fields }] }; // Added 'payloadData' definition for 'nfc' type
+    } else if (type === 'barcode') {
+        payloadData = { value: data.barcodeData };
+    } else {
+        throw new Error('Unknown notification type'); // Changed from 'return reject(new Error(...))' to 'throw'
+    }
 
-        const payload = JSON.stringify({
-            content: 'New scan detected!',
-            embeds: [{ title: embedTitle, color: 5814783, fields: fields }]
-        });
-
-        axios.post(webhookUrl, payload, { headers: { 'Content-Type': 'application/json' } })
-            .then(response => resolve(response.data))
-            .catch(error => reject(error));
-    });
+    const payload = JSON.stringify(payloadData);
+    try {
+        let response = await axios.post(webhookUrl, payload, { headers: { 'Content-Type': 'application/json' } });
+        console.log(response);
+    }
+    catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 module.exports = { sendDiscordWebhook };
